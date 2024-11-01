70f1512d0c0074bf1f13f0d6ae2e8f082c7ab0b9
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2024-11-01 00:30:17 -0700

    Updated gamestate.py with a tick function

--- gamestate.py
+++ gamestate.py
@@ -1,6 +1,6 @@
 """Manages Game state."""
 import sys
-from typing import Dict, List
+from typing import Dict
 from dataclasses import dataclass
 import pygame
 
@@ -27,6 +27,15 @@ class Game:
     clock: pygame.time.Clock
     keymap: Dict[str, str]
     deltaT: float
+    
+    def tick(self) -> Self:
+        """
+        Purpose: Limits FPS. Used for framerate-independent physics.
+        Examples:
+            game.deltaT -> game.clock.tick(game.fps) / 1000
+        """
+        self.deltaT = self.clock.tick(self.fps) / 1000
+        return self
 
     def save(self, file_path: str) -> None:
         """Saves the current game state and writes to the game.csv"""
