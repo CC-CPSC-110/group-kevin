7d69a058cd6d3557546c2235142a8ca5244b426b
Ninja1212 <nima.nazarina@gmail.com>
2024-12-10 19:29:38 -0800

    Moved some things around for neatness/consistency

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 #0 - empty
 #1 - small pellets
@@ -29,3 +30,33 @@ boards = [
     [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 ]
+
+
+pygame.init()
+WIDTH = 900
+HEIGHT = 950
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+level = boards
+color = 'blue'
+PI = math.pi
+flicker = False
+
+
+def draw_board():
+    num1 = ((HEIGHT - 50) // 21)
+    num2 = (WIDTH // 19)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2 and not flicker:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
--- pacman.py
+++ pacman.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
 from character import Character
 from typing import List
-import pygame
-import math
-from board import boards
+
 
 @dataclass
 class Pacman(Character):
@@ -27,32 +25,3 @@ class Pacman(Character):
                     self.direction = 3
                     self.y_position += self.speed
 
-
-pygame.init()
-WIDTH = 900
-HEIGHT = 950
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-timer = pygame.time.Clock()
-fps = 60
-font = pygame.font.Font('freesansbold.ttf', 20)
-level = boards
-color = 'blue'
-PI = math.pi
-flicker = False
-
-
-def draw_board():
-    num1 = ((HEIGHT - 50) // 21)
-    num2 = (WIDTH // 19)
-    for i in range(len(level)):
-        for j in range(len(level[i])):
-            if level[i][j] == 1:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level[i][j] == 2 and not flicker:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
-            if level[i][j] == 3:
-                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
-                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
-            if level[i][j] == 4:
-                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
-                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
